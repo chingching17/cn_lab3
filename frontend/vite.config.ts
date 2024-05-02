ef61c4f58440916c864225283db16e4fa1012c39
chingching17 <73006493+chingching17@users.noreply.github.com>
2024-05-02 06:50:11 +0000

    add dynamic target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://backend:8888',
+//         changeOrigin: true
+//       }
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://backend:8888',
-        changeOrigin: true
+export default ({ mode }) => {
+  const proxyTarget = mode === 'production' ? 'http://localhost:8888' : 'http://backend:8888';
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: proxyTarget,
+          changeOrigin: true
+        }
       }
     }
-  }
-})
+  });
+}
