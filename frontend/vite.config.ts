53fa92535c5e9f7abec36563ba3d466f2ec18e7b
chingching17 <73006493+chingching17@users.noreply.github.com>
2024-05-02 14:49:29 +0000

    fix: dynamic ip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,15 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: 'http://backend:8888',
-//         changeOrigin: true
-//       }
-//     }
-//   }
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  const proxyTarget = mode === 'production' ? 'http://localhost:8888' : 'http://backend:8888';
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: proxyTarget,
-          changeOrigin: true
-        }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.BACKEND_URL, // 'http://backend:8888'
+        changeOrigin: true
       }
     }
-  });
-}
+  }
+})
